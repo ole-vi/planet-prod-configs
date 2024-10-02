f6554d656cbddd58a1bcd7e017cdab8ff63e366f
Mutugi <48474421+Mutugiii@users.noreply.github.com>
2024-10-02 14:30:04 +0300

    Check if db init has been initialized before starting it

--- planet.sh
+++ planet.sh
@@ -12,6 +12,9 @@ if [ -f /srv/planet/pwd/credentials.yml ]; then
   docker compose -f /srv/planet/planet.yml -f /srv/planet/pwd/credentials.yml -p planet $CMD
 else
   docker compose -f /srv/planet/planet.yml -p planet $CMD
+fi
+
+if docker ps -a --format '{{.Names}}' | grep -q '^planet_db-init_1$'; then
   docker wait 'planet_db-init_1'
   docker start 'planet_db-init_1'
 fi
